4c6e08b12dec915029565b58015aeff34c764b3f
Fedya Grigoryan <fgrigoryan@sdvor.com>
2021-07-28 20:00:07 +0500

    YerzhanBot-1 Создать проект и решить какие инструменты будут использованы

---
+++ src/main/java/ru/nop/yerzhanbot/YerzhanBotApplication.java
@@ -0,0 +1,13 @@
+package ru.nop.yerzhanbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YerzhanBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YerzhanBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nop/yerzhanbot/YerzhanBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nop.yerzhanbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YerzhanBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
